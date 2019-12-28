7ec6a4a5fad33e1c9b1b1f4a0af75bc2209eeb11
Kelly Muhindi <kmuhindi@bandahealth.org>
2019-12-28 12:04:04 +0300

    Add update and delete api resources.

--- src/main/java/org/pesi/app/messenger/resource/MessageResource.java
+++ src/main/java/org/pesi/app/messenger/resource/MessageResource.java
@@ -1,8 +1,13 @@
 package org.pesi.app.messenger.resource;
 
+import java.util.Calendar;
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -27,5 +32,29 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Message getMessageWithId(@PathParam("messageId")long messageId) {
 		return mService.getMessage(messageId);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message) {
+		message.setCreated(Calendar.getInstance().getTime());
+		return mService.addMessage(message);
+	}
+	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId")long messageId, Message message) {
+		message.setId(messageId);
+		return mService.updateMessage(message);
+	}
+	
+	@DELETE
+	@Path("/{messageId}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public void deleteMessage(@PathParam("messageId")long messageId) {
+		mService.removeMessage(messageId);
 	}
 }
